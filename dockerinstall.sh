d546de7bbcd0d24f79e1c2726271e2c6603d9768
DAVUTHAN YAMAN <brooqs@gmail.com>
2019-10-31 23:09:12 +0300

    init

---
+++ dockerinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update
+apt upgrade -y
+apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
